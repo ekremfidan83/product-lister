73d527f99ed3bc41be959f58d8d2322573a0fce5
Ekrem Fidan <ekremfidandev@gmail.com>
2025-02-17 17:40:34 +0300

    cross origin eklendi

---
+++ src/main/java/com/ekremdev/productlister/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.ekremdev.productlister.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000", "https://product-list-frontend-jhanj0kp0-ekremfidan83s-projects.vercel.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
